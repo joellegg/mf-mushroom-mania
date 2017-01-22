a111ac037cf8635cbf109921a87acce2a90dceeb
joellegg <joellegg@bellsouth.net>
2017-01-22 13:43:46 -0600

    added controller for the mushrooms and factory for data

--- app/app.js
+++ app/app.js
@@ -5,3 +5,24 @@ angular.module('shroomApp', ['ngRoute'])
       controller: 'RootCtrl',
       templateUrl: '/partials/root.html'
     })
+  })
+  .controller('RootCtrl', function($scope, mushroomFactory){
+    mushroomFactory
+      .getShroomed()
+      .then((value) => {
+        $scope.mushrooms = value;
+        //$scope.description = value.description;
+        console.log('value.data', value)
+      })
+  })
+  .factory('mushroomFactory', ($http) => {
+    return {
+      getShroomed: () => {
+        return $http
+          .get('list.json')
+          .then((value) => {
+            console.log('value.data', value.data)
+          })
+      }
+    }
+  })
